bf564958e791220ed2428b5601960919c76ece74
jason <jason@jason-VirtualBox.(none)>
2014-03-29 17:39:02 -0700

    adding push notification creation

--- PythonServer/notifications/api.py
+++ PythonServer/notifications/api.py
@@ -7,22 +7,76 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.core.serializers.json import DjangoJSONEncoder
 from django.forms.models import model_to_dict
+from accounts.api import pushToNOSQLHash, pushToNOSQLSet
 from accounts.models import Account, AccountLink, Group, AccountSetting, AccountSettings
 from events.models import Event, EventNotification, EventCreatorLocation, EventComment, InvitedFriend
+from notifications.models import Notification
 from rediscli import r as R
 
 logger = logging.getLogger("django.request")
 
 
-def addedFriendPushNotification(recipient, notification):
-	rtn_dict = {'success': False, "msg": ""}
+def createEventNotificationDict(notification):
+	notification_dict = {}
+	try:
+		notification_dict['event'] = notification.event.id
+	except:
+		notification_dict['event'] = ""
+	notification_dict['creator'] = notification_creator
+	notification_dict['message'] = notification.message
+	notification_dict['created'] = str(notification.created)
+	return notification_dict
+
+
+def getNotifications(request):
+	rtn_dict = {'success': False, "msg": "", "notifications": []}
+	try:
+		r = R.r
+		account = Account.objects.get(user=request.user)
+		r_notifications_key = 'account.{0}.notifications.set'.format(account.id)
+		notifications_list = r.zrange(r_notifications_key, 0, 10)
+
+		if not notifications_list:
+			notifications_list = []
+			notifications = Notification.objects.filter(recipients__id=account.id)
+			for notification in notifications:
+				notification_dict = createEventNotificationDict(notification)
+				notifications_list.append(notification_dict)
+				# TODO create task to fill redis with this information
+
+		rtn_dict['notifications'] = notifications_list
+	except Exception as e:
+		logger.info('Error retrieving notifications: {0}'.format(e))
+		rtn_dict['msg'] = 'Error retrieving notifications: {0}'.format(e)
 	return HttpResponse(json.dumps(rtn_dict, cls=DjangoJSONEncoder), content_type="application/json")
 
 
-def eventPushNotification(event, notification):
-	rtn_dict = {'success': False, "msg": ""}
-	return HttpResponse(json.dumps(rtn_dict, cls=DjangoJSONEncoder), content_type="application/json")
+def addedFriendPushNotification(friend, recipient):
+	notification = Notification(notification_creator=friend)
+	notification.message = '{0} has just added you as a friend'.format(friend.user_name)
+	notification.save()
+	r_notification_key = 'account.{0}.notifications.set'.format(recipient.id)
+	notification_string = json.loads(createEventNotificationDict(notification))
+	pushToNOSQLSet(r_notification_key, notification_string, False, 0)
+	sendPushNotification(recipient, notification)
+
+
+def eventPushNotification(event, notification_type, notification_creator):
+	invited_friends = InvitedFriend.objects.filter(event=event)
+	notification = Notification(event=event)
+	message = '{0} changed {1} on event {2}'.format(notification_creator, notification_type, event.name)
+	notification.message = message
+	notification.save()
+	for invited_friend in invited_friends:
+		notification.recipients.add(invited_friend)
+
+	r = R.r
+	for recipient in notification.recipients.all():
+		r_notification_key = 'account.{0}.notifications.set'.format(recipient.id)
+		notification_string = json.loads(createEventNotificationDict(notification))
+		pushToNOSQLSet(r_notification_key, notification_string, False, 0)
+		sendPushNotification(recipient, notification)
+
 
 def sendPushNotification(recipient, notification):
-	rtn_dict = {'success': False, "msg": ""}
-	return HttpResponse(json.dumps(rtn_dict, cls=DjangoJSONEncoder), content_type="application/json")
+	pass
--- PythonServer/notifications/models.py
+++ PythonServer/notifications/models.py
@@ -2,11 +2,15 @@ from django.db import models
 from events.models import *
 from accounts.models import *
 
+
 # Create your models here.
 class Notification(models.Model):
     event = models.ForeignKey(Event, null=True, blank=True)
-    recipient = models.ForeignKey(Account)
+    notification_creator = models.ForeignKey(Account, related_name='notification_creator')
+    notification_recipient = models.ForeignKey(Account, related_name='notification_recipient')
     message = models.CharField(max_length=255, blank=True, null=True)
+    notification_type = models.CharField(max_length=255, blank=True, null=True)
+    recipients = models.ManyToManyField(Account)
     created = models.DateField(auto_now_add=True)
     facebook = models.NullBooleanField(null=True, blank=True, default=False)
     def __unicode__(self):
