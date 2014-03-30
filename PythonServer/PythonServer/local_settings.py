ea02fe24811d72199e0dff838f5258f383ee4fc1
jason <jason@jason-VirtualBox.(none)>
2014-03-30 11:10:49 -0700

    adding some local settings stuff

--- PythonServer/PythonServer/local_settings.py
+++ PythonServer/PythonServer/local_settings.py
@@ -70,7 +70,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = 'static/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -136,6 +136,7 @@ INSTALLED_APPS = (
     'djcelery',
     'accounts',
     'events',
+    'notifications',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
