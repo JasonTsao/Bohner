d1e62cc306704c95965b27c515b7024be5b4da8d
jason <jason@jason-VirtualBox.(none)>
2014-03-30 11:41:41 -0700

    adding a test for serving static files

--- PythonServer/PythonServer/urls.py
+++ PythonServer/PythonServer/urls.py
@@ -11,6 +11,8 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^acct/', include('accounts.urls')),
     url(r'^events/', include('events.urls')),
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', 
+    {'document_root': settings.STATIC_ROOT, 'show_indexes':True}), 
     # Examples:
     # url(r'^$', 'PythonServer.views.home', name='home'),
     # url(r'^PythonServer/', include('PythonServer.foo.urls')),
