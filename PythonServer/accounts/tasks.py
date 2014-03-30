91c3752ecca441532276a538ba4ea709d48cb7b0
jason <jason@jason-VirtualBox.(none)>
2014-03-29 18:36:00 -0700

    code for populating redis with SQL user, group, users groups

--- PythonServer/accounts/tasks.py
+++ PythonServer/accounts/tasks.py
@@ -31,5 +31,45 @@ def populateUserFriends(account_id):
 	except Exception as e:
 		print 'Error populating NOSQL layer with invited friends for event {0}: {1}'.format(event_id, e)
 		return False
+	return True
+
+
+@task
+def populateUser(account_id):
+	try:
+		r = R.r
+		r_user_key = 'account.{0}.hash'.format(user_id)
+		user = Account.objects.get(pk=account_id)
+		r.hmset(model_to_dict(user))
+	except Exception as e:
+		print 'Error populating NOSQL layer with user {0}: {1}'.format(account_id, e)
+		return False
+
+
+@task
+def populateUserGroups(account_id):
+	try:
+		r = R.r
+		r_user_groups_key = 'account.{0}.groups.set'.format(account_id)
+		r.delete(r_user_groups_key)
+		groups = Group.objects.filter(members__id=account_id)
+		for group in groups:
+			pushToNOSQLSet(r_user_groups_key, model_to_dict(group), False, 0)
+	except Exception as e:
+		print 'Error populating NOSQL layer with user {0} groups: {1}'.format(account_id, e)
+		return False
+	return True
+
+
+@task
+def populateGroup(group_id):
+	try:
+		r = R.r
+		r_group_key = 'group.{0}.hash'.format(group_id)
+		group = Group.objects.get(pk=group_id)
+		r.hmset(model_to_dict(group))
+	except Exception as e:
+		print 'Error populating NOSQL layer with group {0}: {1}'.format(group_id, e)
+		return False
 
 	return True
